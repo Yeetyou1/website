4d6b74d9c6a383cd4162d72b4dc9481db5898fc9
Grymmllyn <prettyboring232@gmail.com>
2026-01-14 21:08:18 -0600

    cleaning up

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/* AI Generated */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        versus: ["Versus", "sans-serif"],
+        gsanscode: ["Versus", "sans-serif"],
+      },
+      spacing: {
+        "1080": "1080px",
+      },
+    },
+  },
+  plugins: [],
+};
